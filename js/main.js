64609475aa9310f69b786974e0483935762a54fa
jmkunnat <jmkunnat@umich.edu>
2017-12-05 20:50:48 -0500

    more ai logic with +2 and +4 lol

--- js/main.js
+++ js/main.js
@@ -122,7 +122,7 @@ function player_turn() {
 	if (players[cur_player_index].human) {
 		// highlight playable cards for the player
 
-
+		cur_player_index = (cur_player_index + 1) % players.length;
 	} else {
 		// look in hand first for: color, number, other, then draw
 		// have some other logic for wilds (replace the list_of_cards lookup with just cur_color variable);
@@ -159,23 +159,27 @@ function player_turn() {
 		}
 		// draw a card if we can't play a card, otherwise play it
 		if (to_play == -1) {
-			draw_card(cur_player_index);
+			draw_card(cur_player_index, 1);
 		} else {
 			play_card(cur_player_index, to_play);
 		}
 		// game_over = true;
 	}
-	cur_player_index = (cur_player_index + 1) % players.length;
+	// cur_player_index = (cur_player_index + 1) % players.length;
 	
 }
 
 
-function draw_card(player_index) {
-	for (var i = 0; i < players[cur_player_index].hand.length; i++) {
-		//.console.log(list_of_cards[players[cur_player_index].hand[i]].color + " " + list_of_cards[players[cur_player_index].hand[i]].number + " " + list_of_cards[players[cur_player_index].hand[i]].special);
+function draw_card(player_index, num_to_draw) {
+	// TODO add in actual loop  that draws multiple cards
+	// for (var i = 0; i < players[cur_player_index].hand.length; i++) {
+	// 	//.console.log(list_of_cards[players[cur_player_index].hand[i]].color + " " + list_of_cards[players[cur_player_index].hand[i]].number + " " + list_of_cards[players[cur_player_index].hand[i]].special);
+	// }
+	for (var i = 0; i < num_to_draw; i++) {
+		players[player_index].hand.push(deck.pop());
 	}
-	players[player_index].hand.push(deck.pop());
 	console.log("draw a card bro");
+	cur_player_index = (cur_player_index + 1) % players.length;
 }
 
 function play_card(cur_player_index, loc_in_hand) {
@@ -193,19 +197,31 @@ function play_card(cur_player_index, loc_in_hand) {
 	}
 	
 	players[cur_player_index].hand.splice(loc_in_hand, 1);
+
+	// write in the case for +2 or + 4
 	if (players[cur_player_index].hand.length == 1) {
 		console.log("UNO!");
 	} else if (players[cur_player_index].hand.length == 0) {
 		console.log("game over");
 		game_over = true;
 	}
+	if (list_of_cards[last_played_card].special == "draw-2") {
+		console.log("next player draws 2");
+		draw_card((cur_player_index + 1) % players.length, 2);
+		cur_player_index = (cur_player_index + 1) % players.length;
+	} else if (list_of_cards[last_played_card].special == "wild-draw-4") {
+		console.log("next player draws 4");
+		draw_card((cur_player_index + 1) % players.length, 4);
+		cur_player_index = (cur_player_index + 1) % players.length;
+	}
+	cur_player_index = (cur_player_index + 1) % players.length;
 }
 
 function get_color(cur_player_index) {
 	if (players[cur_player_index].human) {
 		// have a pop up ask them for color
 	} else {
-		// just choose red for rn
+		// just choose red for rn - run through and choose the color with most 
 		return "red";
 	}
 }
