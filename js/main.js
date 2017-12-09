40e6cf0817bd663f1b3a7e4477bee4cd88a9d2b9
Sagar Singichetti <singichs@umich.edu>
2017-12-09 18:31:30 -0500

    timoeut on drag animation

--- js/main.js
+++ js/main.js
@@ -266,13 +266,19 @@ function draw_card(player_index, num_to_draw) {
 	}
 	// console.log(players);
 	// console.log(players[3].hand)
+	draw_animation(player_index);
+	setTimeout(delay_display_continue(player_index, num_to_draw), 2000);
+	//delay_display_continue(player_index, num_to_draw);
+}
+
+function delay_display_continue(player_index, num_to_draw){
 	let p4hand = document.getElementById('player4-cards');
 	p4hand.innerHTML = "";
-	draw_animation(player_index);
 	display_cards();
 	// console.log("cards in hand for " + players[cur_player_index].name + " " + players[cur_player_index].hand.length);
 	console.log(players[player_index].name + " drew " + num_to_draw + " cards");
 	get_next_player();
+	play_game();
 }
 
 function play_card(loc_in_list) {
@@ -520,7 +526,7 @@ function update_playable_cards(human_index) {
 		// console.log(card_li);
 		if (player_card_valid(players[human_index].hand[i])) {
 
-			console.log("card" + i + " playable")
+			//console.log("card" + i + " playable")
 			// add the draggable class
 			card_li.addClass("draggable");
 			card_li.draggable( "enable" );
@@ -558,7 +564,7 @@ function player_card_valid(player_card_index) {
 
 window.setInterval(function(){
 
-name = players[cur_player_index].name;
+var name = players[cur_player_index].name;
 
 if (name == "human boi")
 {
